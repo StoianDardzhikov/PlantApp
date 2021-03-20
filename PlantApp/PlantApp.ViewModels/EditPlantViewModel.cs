4c369b744c8356034041a297ccf33730fc101af8
StoianDardzhikov <stoiand5858@gmail.com>
2021-03-20 15:11:39 +0200

    Removed unnecessary code

--- PlantApp/PlantApp.ViewModels/EditPlantViewModel.cs
+++ PlantApp/PlantApp.ViewModels/EditPlantViewModel.cs
@@ -1,13 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
-
-namespace PlantApp.ViewModels
+﻿namespace PlantApp.ViewModels
 {
     public class EditPlantViewModel
     {
         public int PlantId { get; set; }
         public string Name { get; set; }
         public int WateringPeriod { get; set; }
-
-        public ModelStateDictionary ModelStateErrors { get; set; }
     }
 }
